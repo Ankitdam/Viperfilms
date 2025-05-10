f801bcf18f3005d2b83b65ad7563a3f7c0c14bee
Ankit malhotra <96262310+Ankitdam@users.noreply.github.com>
2025-05-10 11:20:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -89,7 +89,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/viper_film')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/viper_ott_stores')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/viperflimsbackup')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'viper_film') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/team_vipers')
 
@@ -101,7 +101,7 @@ MAX_BTN = bool(environ.get('MAX_BTN', True))
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 IMDB = bool(environ.get('IMDB', False))
 AUTO_FFILTER = bool(environ.get('AUTO_FFILTER', True))
-AUTO_DELETE = bool(environ.get('AUTO_DELETE', True))
+AUTO_DELETE = bool(environ.get('AUTO_DELETE', False))
 LONG_IMDB_DESCRIPTION = bool(environ.get("LONG_IMDB_DESCRIPTION", False))
 SPELL_CHECK_REPLY = bool(environ.get("SPELL_CHECK_REPLY", True))
 MELCOW_NEW_USERS = bool(environ.get('MELCOW_NEW_USERS', True))
